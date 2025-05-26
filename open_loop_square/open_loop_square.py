daded854b349c62e07afb9c1c295065b343cf29f
ubuntu <singhprabhjot4553@gmail.com>
2025-05-27 00:56:11 +1000

    commit

--- open_loop_square/open_loop_square.py
+++ open_loop_square/open_loop_square.py
@@ -3,60 +3,70 @@
 import rospy
 from duckietown_msgs.msg import Twist2DStamped
 from duckietown_msgs.msg import FSMState
- 
+
 class Drive_Square:
     def __init__(self):
-        #Initialize global class variables
         self.cmd_msg = Twist2DStamped()
 
-        #Initialize ROS node
         rospy.init_node('drive_square_node', anonymous=True)
-        
-        #Initialize Pub/Subs
-        self.pub = rospy.Publisher('/akandb/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
-        rospy.Subscriber('/akandb/fsm_node/mode', FSMState, self.fsm_callback, queue_size=1)
-        
-    # robot only moves when lane following is selected on the duckiebot joystick app
+
+        self.pub = rospy.Publisher('/jaatt/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
+        rospy.Subscriber('/jaatt/fsm_node/mode', FSMState, self.fsm_callback, queue_size=1)
+
+        self.has_moved = False
+
     def fsm_callback(self, msg):
         rospy.loginfo("State: %s", msg.state)
         if msg.state == "NORMAL_JOYSTICK_CONTROL":
             self.stop_robot()
-        elif msg.state == "LANE_FOLLOWING":            
-            rospy.sleep(1) # Wait for a sec for the node to be ready
+        elif msg.state == "LANE_FOLLOWING" and not self.has_moved:
+            rospy.sleep(1)
             self.move_robot()
- 
-    # Sends zero velocities to stop the robot
+            self.has_moved = True
+
     def stop_robot(self):
         self.cmd_msg.header.stamp = rospy.Time.now()
         self.cmd_msg.v = 0.0
         self.cmd_msg.omega = 0.0
         self.pub.publish(self.cmd_msg)
- 
-    # Spin forever but listen to message callbacks
-    def run(self):
-    	rospy.spin() # keeps node from exiting until node has shutdown
 
-    # Robot drives in a square and then stops
-    def move_robot(self):
-
-        #YOUR CODE GOES HERE#
+    def move_forward(self, speed, duration):
         self.cmd_msg.header.stamp = rospy.Time.now()
-        self.cmd_msg.v = 0.5 # striaght line velocity
+        self.cmd_msg.v = speed
         self.cmd_msg.omega = 0.0
         self.pub.publish(self.cmd_msg)
-        rospy.loginfo("Forward!")
-        rospy.sleep(1) # straight line driving time
-        
-        self.cmd_msg.header.stamp = rospy.Time.now()
-        self.cmd_msg.v = -0.5 # striaght line velocity
-        self.cmd_msg.omega = 0.0
-        self.pub.publish(self.cmd_msg)
-        rospy.loginfo("Backward!")
-        rospy.sleep(1) # straight line driving time
-        
-        ######################
-                
+        rospy.sleep(duration)
         self.stop_robot()
+        rospy.sleep(0.5)
+
+    def turn_90_degrees(self, angular_speed, duration):
+        self.cmd_msg.header.stamp = rospy.Time.now()
+        self.cmd_msg.v = 0.0
+        self.cmd_msg.omega = angular_speed
+        self.pub.publish(self.cmd_msg)
+        rospy.sleep(duration)
+        self.stop_robot()
+        rospy.sleep(0.5)
+
+    def move_robot(self):
+        forward_speed = 0.3
+        forward_time = 3.3  # ~1 meter at 0.3 m/s
+
+        angular_speed = 1.0
+        turn_time = 1.57  # ~90 degrees at 1.0 rad/s
+
+        for i in range(4):
+            rospy.loginfo(f"Side {i+1}: Moving forward")
+            self.move_forward(forward_speed, forward_time)
+
+            rospy.loginfo(f"Corner {i+1}: Turning 90 degrees")
+            self.turn_90_degrees(angular_speed, turn_time)
+
+        rospy.loginfo("Finished square")
+        self.stop_robot()
+
+    def run(self):
+        rospy.spin()
 
 if __name__ == '__main__':
     try:
